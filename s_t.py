6d3e5b01aa4aa0c7c6d9a06d11f83df2c0dc38ca
valent4 <vg118664@gmail.com>
2025-05-01 19:00:05 -0500

    Update s_t.py

--- s_t.py
+++ s_t.py
@@ -19,7 +19,7 @@ st.title("TRADUCTOR.")
 st.subheader("Escucho lo que quieres traducir.")
 
 
-image = Image.open('OIG7.jpg')
+image = Image.open('traductor.jpg')
 
 st.image(image,width=300)
 with st.sidebar:
